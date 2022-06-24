847dcd0d6e06e91d8fb3007b011dc8b611abcbf1
Tyler Williams <70001770+SinceWhenBro@users.noreply.github.com>
2022-06-24 16:59:33 -0700

    Add files via upload

---
+++ Williams_Tyler_ArtistManagementSystem_CaseStudy/src/main/java/com/example/Williams_Tyler_ArtistManagementSystem_CaseStudy/services/ArtistService.java
@@ -0,0 +1,15 @@
+package com.example.Williams_Tyler_ArtistManagementSystem_CaseStudy.services;
+
+import com.example.Williams_Tyler_ArtistManagementSystem_CaseStudy.models.Artist;
+
+import java.util.List;
+
+public interface ArtistService {
+
+    List<Artist> getAllArtists();
+    void saveArtist(Artist artist);
+    Artist getArtistById(long id);
+
+    void deleteArtistById(long id);
+
+}
